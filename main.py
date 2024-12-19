3670fe59c67a53b5d66956f2e6284b0c8929ac05
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-12-19 11:38:31 +0900

    [add] 距離の測定、送信

--- main.py
+++ main.py
@@ -2,31 +2,61 @@ import bluetooth
 import time
 from ble_simple_peripheral import BLESimplePeripheral
 import machine
+from machine import Pin
 
+# 距離センサー設定
+trigger_pin = 4
+echo_pin = 5
+trigger = Pin(trigger_pin, Pin.OUT)
+echo = Pin(echo_pin, Pin.IN)
 
+def read_distance():
+    # トリガーピンをHIGH
+    trigger.high()
+    time.sleep_us(11)
+    trigger.low()
 
-sensor_temp = machine.ADC(4)
-conversion_factor = 3.3 / 65535
+    # echoがHIGHになるまで待つ
+    while (echo.value() == 0):
+        pass
 
-    
+    lastreadtime = time.ticks_us() 
+
+    # echoがLOWになるまで待つ(パルス継続時間を測定)
+    while (echo.value() == 1):
+        pass
+
+    echotime = time.ticks_us() - lastreadtime
+
+    # echotimeが大きいときは障害物なしとみなす
+    if echotime > 37000:
+        return None
+    else:
+        distance = (echotime * 0.034) / 2
+        distance = round(distance, 1)
+        return distance
 
 def main():
-    # BLE初期化
     ble = bluetooth.BLE()
     peripheral = BLESimplePeripheral(ble, name="お湯溜りボンゴ")
 
     try:
         while True:
             if peripheral._connections:
-                reading = sensor_temp.read_u16() * conversion_factor
-                temp = 27 - (reading - 0.706)/0.001721
-                temp = round(temp, 1)
-                print(temp)
-                peripheral.send(str(temp).encode())  # データをバイト列に変換して送信
-                print("Data sent.")
+                dist = read_distance()
+
+                if dist is None:
+                    dist_str = "No obstacle"
+                else:
+                    dist_str = f"{dist}"
+
+                data_str = f"{dist_str}"
+                peripheral.send(data_str.encode())
+                print("Data sent:", data_str)
             else:
                 print("No connections. Waiting...")
-            time.sleep(1) # 5秒ごとに送信
+
+            time.sleep(1)
     except KeyboardInterrupt:
         print("Program interrupted.")
 
